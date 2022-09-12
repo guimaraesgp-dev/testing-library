cb773f958218af38d24e970e3dad03d7b646e6a1
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-09-12 10:33:19 -0300

    REQUSITO 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('testes do componente App', () => {
+  test('É exibido na tela um link com o texto Home', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  test('É exibido na tela um link com o texto About', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  test('É exibido na tela um link com o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
